bfc758a6f09e3ff514983a9601a27aecda727868
Jun Lee <jlee@notesquare.co.kr>
2023-05-12 05:06:53 +0000

    Add Apptainer support

--- plexsim_initializer/backends/__init__.py
+++ plexsim_initializer/backends/__init__.py
@@ -9,7 +9,18 @@ def initialize(input_fp, out_fp, **kwargs):
     sim_backend = sim_config.get('backend', 'local')
     if sim_backend == 'mpi':
         from .mpi.sim import initialize as MPI_initialize
-        MPI_initialize(input_fp, out_fp, **kwargs)
+        from mpi4py import MPI
+
+        if MPI.COMM_WORLD.Get_size() > 1:
+            # static mode, mpi4py.futures is required
+            from mpi4py.futures._lib import SharedPoolCtx
+            with SharedPoolCtx() as context:
+                if context is not None:
+                    MPI_initialize(input_fp, out_fp, **kwargs)
+        else:
+            # dynamic mode
+            MPI_initialize(input_fp, out_fp, **kwargs)
+
     elif sim_backend == 'local':
         from .local.sim import initialize as local_initialize
         local_initialize(input_fp, out_fp, **kwargs)
