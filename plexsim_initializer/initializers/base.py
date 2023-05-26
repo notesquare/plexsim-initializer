f0ffc305a05ff5e12e78cbdc7e9dc371e500e44f
kyungtae <kyungtae@kittoimac.local>
2023-05-26 16:01:41 +0900

    Boundary 관련 변경 및 입력 자료 업데이트
    - external, induced field 고정해야 할 좌표값을 각각 입력으로 받도록 수정
    - external field: 경계면 포함하여 값 0으로 고정
    - induced field: 경계면 제외하고 값 0으로 고정
    - state 계산 시 constant external field 좌표의 값을 0으로 업데이트
    - 줄어든 grid shape에 맞도록 입력 자료 업데이트(Triassic)

--- plexsim_initializer/initializers/base.py
+++ plexsim_initializer/initializers/base.py
@@ -79,19 +79,33 @@ class BaseInitializer:
 
         self.gilbert_curve = [tuple(coord) for coord in gilbert_curve]
 
-        constant_field_node = _environment_config.get(
-            'constant_field_node_coords')
-        if constant_field_node is not None:
-            constant_field_node = \
-                self.load_relative_npy_file(constant_field_node)
-        self.constant_field_node = constant_field_node
+        constant_external_field_node = _environment_config.get(
+            'constant_external_field_node_coords')
+        if constant_external_field_node is not None:
+            constant_external_field_node = \
+                self.load_relative_npy_file(constant_external_field_node)
+        self.constant_external_field_node = constant_external_field_node
 
-        constant_field_center = _environment_config.get(
-            'constant_field_center_coords')
-        if constant_field_center is not None:
-            constant_field_center = \
-                self.load_relative_npy_file(constant_field_center)
-        self.constant_field_center = constant_field_center
+        constant_induced_field_node = _environment_config.get(
+            'constant_induced_field_node_coords')
+        if constant_induced_field_node is not None:
+            constant_induced_field_node = \
+                self.load_relative_npy_file(constant_induced_field_node)
+        self.constant_induced_field_node = constant_induced_field_node
+
+        constant_external_field_center = _environment_config.get(
+            'constant_external_field_center_coords')
+        if constant_external_field_center is not None:
+            constant_external_field_center = \
+                self.load_relative_npy_file(constant_external_field_center)
+        self.constant_external_field_center = constant_external_field_center
+
+        constant_induced_field_center = _environment_config.get(
+            'constant_induced_field_center_coords')
+        if constant_induced_field_center is not None:
+            constant_induced_field_center = \
+                self.load_relative_npy_file(constant_induced_field_center)
+        self.constant_induced_field_center = constant_induced_field_center
 
         self.create_dataset_kwargs = dict(
             chunks=True, shuffle=True,
@@ -221,15 +235,33 @@ class BaseInitializer:
                                 data=np.array(self.gilbert_curve),
                                 dtype=np.int16)
 
-        if self.constant_field_node is not None:
-            settings.create_dataset('constant_field_node_coords',
-                                    data=np.array(self.constant_field_node),
-                                    dtype=np.int16)
+        if self.constant_external_field_node is not None:
+            settings.create_dataset(
+                'constant_external_field_node_coords',
+                data=np.array(self.constant_external_field_node),
+                dtype=np.int16
+            )
 
-        if self.constant_field_center is not None:
-            settings.create_dataset('constant_field_center_coords',
-                                    data=np.array(self.constant_field_center),
-                                    dtype=np.int16)
+        if self.constant_induced_field_node is not None:
+            settings.create_dataset(
+                'constant_induced_field_node_coords',
+                data=np.array(self.constant_induced_field_node),
+                dtype=np.int16
+            )
+
+        if self.constant_external_field_center is not None:
+            settings.create_dataset(
+                'constant_external_field_center_coords',
+                data=np.array(self.constant_external_field_center),
+                dtype=np.int16
+            )
+
+        if self.constant_induced_field_center is not None:
+            settings.create_dataset(
+                'constant_induced_field_center_coords',
+                data=np.array(self.constant_induced_field_center),
+                dtype=np.int16
+            )
 
     def base_path(self, h5f, iteration):
         return np.string_(h5f.attrs['basePath']).replace(
@@ -312,15 +344,26 @@ class BaseInitializer:
         self.B_external = self.load_field_center(
             self.environment_config.get('external_magnetic_field'),
             field_dtype)
+        self.B_external[tuple(
+            axis for axis in self.constant_external_field_center.T)] = 0
+
         self.B_induced = self.load_field_center(
             self.environment_config.get('induced_magnetic_field', [0, 0, 0]),
             field_dtype)
+        self.B_induced[tuple(
+            axis for axis in self.constant_induced_field_center.T)] = 0
+
         self.E_external = self.load_field_node(
             self.environment_config.get('external_electric_field'),
             field_dtype)
+        self.E_external[tuple(
+            axis for axis in self.constant_external_field_node.T)] = 0
+
         self.E_induced = self.load_field_node(
             self.environment_config.get('induced_electric_field', [0, 0, 0]),
             field_dtype)
+        self.E_induced[tuple(
+            axis for axis in self.constant_induced_field_node.T)] = 0
 
         self.write_B(fields)
         self.write_E(fields)
@@ -893,9 +936,9 @@ class BaseInitializer:
             n_computational_to_physical = \
                 grid_values['n_computational_to_physical']
 
-            grid_n = np.zeros((self.grid_shape + 1), dtype=np.float32)
-            grid_U = np.zeros((*(self.grid_shape + 1), 3), dtype=np.float32)
-            grid_U2 = np.zeros((self.grid_shape + 1), dtype=np.float32)
+            grid_n = np.zeros((self.grid_shape + 1), dtype=np.float64)
+            grid_U = np.zeros((*(self.grid_shape + 1), 3), dtype=np.float64)
+            grid_U2 = np.zeros((self.grid_shape + 1), dtype=np.float64)
 
             compute_grid_velocity(X, U, C_idx, grid_n, grid_U, grid_U2)
 
@@ -912,6 +955,13 @@ class BaseInitializer:
 
             grid_n = grid_n * n_computational_to_physical\
                 / self.cell_size.prod()
+
+            grid_n[tuple(axis for axis in
+                         self.constant_external_field_node.T)] = 0
+            grid_U[tuple(axis for axis in
+                         self.constant_external_field_node.T)] = 0
+            grid_T[tuple(axis for axis in
+                         self.constant_external_field_node.T)] = 0
 
             particle_name = grid_values['particle_name']
             self.write_state(fields_group, particle_name, axis_labels,
