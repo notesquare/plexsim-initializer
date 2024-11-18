f29783478700b8d13d09a70bd762be4b51bc7a9c
KTLee <jeff8287@gmail.com>
2024-11-18 00:43:20 +0000

    mpi4py version 의존성 문제 수정

--- plexsim_initializer/backends/mpi/common.py
+++ plexsim_initializer/backends/mpi/common.py
@@ -1,12 +1,59 @@
 import os
+import sys
 
 from mpi4py import MPI
 from mpi4py.futures import MPIPoolExecutor
-from mpi4py.futures import _lib
-# monkey patch get_python_flags function
-_get_python_flags = _lib.get_python_flags
-_lib.get_python_flags = lambda: []
-from mpi4py.futures._lib import get_max_workers, get_python_flags, get_spawn_module  # noqa
+
+
+FLAG_OPT_MAP = {
+    'debug': 'd',
+    'inspect': 'i',
+    'interactive': 'i',
+    'optimize': 'O',
+    'dont_write_bytecode': 'B',
+    'no_user_site': 's',
+    'no_site': 'S',
+    'ignore_environment': 'E',
+    'verbose': 'v',
+    'bytes_warning': 'b',
+    'quiet': 'q',
+    'hash_randomization': 'R',
+    'isolated': 'I',
+    # 'dev_mode': 'Xdev',
+    # 'utf8_mode': 'Xutf8',
+    # 'warn_default_encoding': 'Xwarn_default_encoding',
+    'safe_path': 'P',
+    # 'int_max_str_digits': 'Xint_max_str_digits=0'
+}
+
+
+def get_python_flags():
+    args = []
+    for flag, opt in FLAG_OPT_MAP.items():
+        val = getattr(sys.flags, flag, 0)
+        val = val if opt[0] != 'i' else 0
+        val = val if opt[0] != 'Q' else min(val, 1)
+        if val > 0:
+            args.append('-' + opt * val)
+    for opt in sys.warnoptions:  # pragma: no cover
+        args.append('-W' + opt)
+    sys_xoptions = getattr(sys, '_xoptions', {})
+    for opt, val in sys_xoptions.items():  # pragma: no cover
+        args.append('-X' + opt if val is True else
+                    '-X' + opt + '=' + val)
+    return args
+
+
+def get_max_workers():
+    max_workers = os.environ.get('MAX_WORKERS')
+    if max_workers is not None:
+        return int(max_workers)
+    if MPI.UNIVERSE_SIZE != MPI.KEYVAL_INVALID:  # pragma: no branch
+        universe_size = MPI.COMM_WORLD.Get_attr(MPI.UNIVERSE_SIZE)
+        if universe_size is not None:  # pragma: no cover
+            world_size = MPI.COMM_WORLD.Get_size()
+            return max(universe_size - world_size, 1)
+    return 1
 
 
 def gen_arg(arg, n_workers):
@@ -40,7 +87,7 @@ class MPIInitializer:
                 'python_exe': "apptainer",
                 'python_args': [
                     '--silent', 'exec', os.environ['APPTAINER_CONTAINER'],
-                    'python', *_get_python_flags()
+                    'python', *get_python_flags()
                 ]
             }
         elif 'SINGULARITY_CONTAINER' in os.environ:
@@ -48,7 +95,7 @@ class MPIInitializer:
                 'python_exe': 'singularity',
                 'python_args': [
                     '--silent', 'exec', os.environ['SINGULARITY_CONTAINER'],
-                    'python', *_get_python_flags()
+                    'python', *get_python_flags()
                 ]
             }
 
